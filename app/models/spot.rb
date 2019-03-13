0f55a0b339670d44238e3b5af8aad3b1628d3de8
Avalon van der Horst <avalonvdhorst@gmail.com>
2019-03-13 17:55:17 +0100

    view of the spot show

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -5,4 +5,28 @@ class Spot < ApplicationRecord
   has_many :advices
   has_many :photos
   has_many :difficulty_levels
+
+  def average_rating
+    sum = 0
+    self.reviews.each do |review|
+      sum += review.rating
+    end
+    return sum / self.reviews.count
+  end
+
+  def pros
+    self.advices.where(kind: true)
+  end
+
+  def cons
+    self.advices.where(kind: false)
+  end
 end
+
+# class Avalon
+#   def say_hi
+#     puts "Hallo!!! :)"
+#   end
+# end
+# avalon = Avalon.new
+# avalon.say_hi
