8a5efeb830577914265f062fcdb1eedb9a973b40
Avalon van der Horst <avalonvdhorst@gmail.com>
2019-03-13 11:44:19 +0100

    photocontroller finished

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,33 @@
+class PhotosController < ApplicationController
+  def new
+    @photo = Photo.new
+    @spot = Spot.find(params[:id])
+  end
+
+  def create
+    if params[:photo].nil? && params["commit"] != "Done"
+      redirect_to new_spot_photo_path(@spot)
+      return
+    elsif params[:photo].nil? && params["commit"] == "Done"
+      redirect_to spot_path(@spot)
+      return
+    end
+    @photo = Photo.new(photo_params)
+    @spot = Spot.find(@spot)
+    @photo.spot_id = @spot
+    if @photo.save
+      params["commit"] == "Done" ? (redirect_to spot_path(@spot)) : (redirect_to new_spot_photo_path(@spot))
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:url)
+  end
+end
